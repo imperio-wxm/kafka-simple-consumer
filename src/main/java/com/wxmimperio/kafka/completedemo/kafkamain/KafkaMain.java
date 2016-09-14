b3fe8dd8d7f22e7750b8dd853e2dd233cd0ca972
wxmimperio <wxmimperio@outlook.com>
2016-09-14 16:16:00 +0800

    "获取每个topic中的partition id"

--- src/main/java/com/wxmimperio/kafka/completedemo/kafkamain/KafkaMain.java
+++ src/main/java/com/wxmimperio/kafka/completedemo/kafkamain/KafkaMain.java
@@ -5,6 +5,7 @@ import com.wxmimperio.kafka.completedemo.model.ConsumerData;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -35,21 +36,25 @@ public class KafkaMain implements Runnable {
     @Override
     public void run() {
 
-        List<String> brokers = Collections.singletonList("192.168.18.35:9092");
-        long curOffset = 1235556;
+        List<String> brokers = new ArrayList<String>();
+        //brokers.add("10.1.8.207:9092");
+        //brokers.add("10.1.8.206:9092");
+        //brokers.add("10.1.8.208:9092");
+        brokers.add("192.168.18.35:9092");
+        long curOffset = 100;
         while (true) {
             KafkaConsumer c = null;
             try {
-                c = new KafkaConsumer("topic_1", 0, curOffset, brokers);
+                c = new KafkaConsumer("topic_002", 1, curOffset, brokers);
                 c.open();
                 ConsumerData data = c.fetch();
                 if (data != null) {
                     curOffset = data.getOffset();
                     //byte[] message = data.getDatas();
-                    /*System.out.println(data.getOffset());
+                    System.out.println(data.getOffset());
                     for (byte[] message : data.getDatas()) {
                         System.out.println("======" + new String(message));
-                    }*/
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- src/test/java/com/wxmimperio/kafka/findnewleader/FindNewLeaderTest.java
+++ src/test/java/com/wxmimperio/kafka/findnewleader/FindNewLeaderTest.java
@@ -1,6 +1,12 @@
 package com.wxmimperio.kafka.findnewleader;
 
+import kafka.javaapi.PartitionMetadata;
+import kafka.javaapi.TopicMetadata;
+import kafka.javaapi.TopicMetadataRequest;
+import kafka.javaapi.consumer.SimpleConsumer;
 import org.junit.Test;
+
+import java.util.*;
 
 /**
  * Created by weiximing.imperio on 2016/9/5.
@@ -23,5 +29,57 @@ public class FindNewLeaderTest {
         FindNewLeader2 findNewLeader2 = new FindNewLeader2();
         String newLeader = findNewLeader2.getLeaderBroderName();
         System.out.println(newLeader);
+    }
+
+    @Test
+    public void getPartitionMetadata() {
+
+        Map<String, Integer> brokers = new HashMap<String, Integer>();
+        List<String> brokerAddrs = new ArrayList<String>();
+        brokerAddrs.add("192.168.18.35:9092");
+
+        List<Integer> partitionList = new ArrayList<Integer>();
+
+        for (String addr : brokerAddrs) {
+            String[] arr = addr.split(":");
+            brokers.put(arr[0], Integer.parseInt(arr[1]));
+        }
+
+        PartitionMetadata partitionMeta = null;
+        List<String> topics = Collections.singletonList("topic_002");
+
+        boolean find = false;
+        for (String broker : brokers.keySet()) {
+            SimpleConsumer leaderSearcher = new SimpleConsumer(broker, brokers.get(broker), 100000, 64 * 1024, "leaderLookup");
+            try {
+                TopicMetadataRequest req = new TopicMetadataRequest(topics);
+                kafka.javaapi.TopicMetadataResponse resp = leaderSearcher.send(req);
+                List<TopicMetadata> metaData = resp.topicsMetadata();
+                for (TopicMetadata item : metaData) {
+                    for (PartitionMetadata part : item.partitionsMetadata()) {
+                        partitionList.add(part.partitionId());
+                        /*if (part.partitionId() == this.partition) {
+                            partitionMeta = part;
+                            find = true;
+                            break;
+                        }*/
+                    }
+                    /*if (find) {
+                        break;
+                    }*/
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                leaderSearcher.close();
+            }
+            if (find) {
+                break;
+            }
+        }
+
+        for(int partitonId : partitionList) {
+            System.out.println("partition id = " + partitonId);
+        }
     }
 }
